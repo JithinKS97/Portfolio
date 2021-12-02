483ca46c90cbc7276234c6617da5420aa4058c4b
Jithin KS <jithinks@Jithins-MacBook-Pro.local>
2021-12-02 20:46:31 +0530

    portfolio

--- components/Home/index.js
+++ components/Home/index.js
@@ -1,7 +1,15 @@
 /* eslint-disable @next/next/no-img-element */
 /* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable react/no-unescaped-entities */
-import { Box, Text, Flex, Center, ScaleFade } from "@chakra-ui/react";
+import {
+  Box,
+  Text,
+  Flex,
+  Center,
+  ScaleFade,
+  Link,
+  HStack,
+} from "@chakra-ui/react";
 
 export default function Home() {
   return (
@@ -33,7 +41,28 @@ export default function Home() {
                 Lately I have been exploring the space of web 3.0 and crypto a
                 lot...
               </Text>
-              <Text color="white" mt="10" fontSize="20px"></Text>
+              <Box mt="10">
+                <Link
+                  target="_blank"
+                  fontSize="20px"
+                  fontWeight="bold"
+                  color="#f15bb5"
+                  href="https://rxresu.me/r/wv3nlu"
+                >
+                  See resume
+                </Link>
+                <HStack mt="5">
+                  <Box fontSize="20px" fontWeight="bold" color="#FFD118">
+                    Find me at
+                  </Box>
+                  <Link target="_blank" href="https://github.com/JithinKS97">
+                    <img className="logo" src="octocat.png"></img>
+                  </Link>
+                  <Link target="_blank" href="https://twitter.com/JithinKS_">
+                    <img className="logo-twitter" src="twitter.png"></img>
+                  </Link>
+                </HStack>
+              </Box>
             </Box>
           </ScaleFade>
         </Center>
@@ -51,5 +80,33 @@ const style = `
   .image {
     width: 300px;
     border-radius: 100%;
+  }
+  .logo {
+    width:70px;
+    cursor: pointer;
+  }
+  .logo-twitter {
+    width:40px;
+    cursor: pointer;
+    margin-left:20px;
+    margin-top:15px;
+  }
+  .logo:hover {
+    filter: brightness(150%);
+    -webkit-filter: brightness(150%);
+    -moz-filter: brightness(150%);
+    -o-filter: brightness(150%);
+    -ms-filter: brightness(150%);
+    -webkit-transition: all 0.50s;
+    transition: all 0.10s;
+  }
+  .logo-twitter:hover {
+    filter: brightness(150%);
+    -webkit-filter: brightness(150%);
+    -moz-filter: brightness(150%);
+    -o-filter: brightness(150%);
+    -ms-filter: brightness(150%);
+    -webkit-transition: all 0.50s;
+    transition: all 0.10s;
   }
 `;
